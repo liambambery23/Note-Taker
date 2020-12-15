a0d45bc65f8ce46ae0f6be231e5edbf53d30ae48
Liam B <71141670+liambambery23@users.noreply.github.com>
2020-12-15 15:35:39 -0500

    added get and post routes for server

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,12 @@
 const noteData = require("../db/db.json");
 
+module.exports = function(app) {
+
+    app.get("/api/notes", function (req, res) {
+        res.json(noteData);
+    });
+
+    app.post("/api/notes", function (req, res) {
+        noteData.push(req.body)
+    })
+};
