38356db76d297ddf269084bdd1f068e2ff6734c5
Liam B <71141670+liambambery23@users.noreply.github.com>
2020-12-18 17:17:50 -0500

    removed unneeded comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -26,20 +26,12 @@ module.exports = function(app) {
         res.json(noteData[req.params.id]);
     });
 
-   // DELETE `/api/notes/:id` - 
-   //Should receive a query parameter containing the id of a note to delete. 
-   //This means you'll need to find a way to give each note a unique `id` when 
-   //it's saved.
-   //1.read all notes from the `db.json` file, 
-   //2. remove the note with the given `id` property,
-   //3. then rewrite the notes to the `db.json` file.
+   
    app.delete("/api/notes/:id", function(req, res) {
        console.log(req.params.id);
-       //1. nodeData
-       //2. how do I remove stuff? locate id
+        //locate id
        for (var i = 0; i < noteData.length; i++) {
         if (req.params.id === noteData[i].id) {
-            //we found it
             //toss it out
             noteData.splice(i, 1);
             console.log(noteData)
